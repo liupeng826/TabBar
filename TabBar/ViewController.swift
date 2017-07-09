e7c4a09003116161870f5b8807e3b5d335ae9294
LiuPeng <liupeng826@hotmail.com>
2017-07-09 16:43:02 +0800

    Initial Commit

---
+++ TabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBar
+//
+//  Created by v0cn123 on 09/07/2017.
+//  Copyright © 2017 v0cn123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
